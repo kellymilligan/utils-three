eaa15eb0fc107a491d8239cbca45517c7b8b733d
Kelly Milligan <kelly@milligan.net.nz>
2017-06-09 14:42:35 +0100

    Update curve path from points util

--- curve_path_from_points.js
+++ curve_path_from_points.js
@@ -1,34 +1,56 @@
 /*
     Extension helper for generating a CurvePath from an array of supplied points.
     Accepts an array of [Vector3] points to contruct the path from.
+    Note: Currently assumes a closed path, and uses the first point as the final.
     ---
 
     points      Array       List of Vector3 points
-    scale       Number      Scale factor
+    width       Number      optional - Width of the path, useful when using normalised X
+    height      Number      optional - Height of the path, useful when using normalised Y
+    scale       Number      optional - Scale factor
 
 */
 
 import { Vector3, LineCurve3, CurvePath } from 'three';
+import {PI, TWO_PI, HALF_PI} from '../math/constants';
 
-export default class CurvePathFromPoints extends CurvePath {
+export default class CustomCurvePath extends CurvePath {
 
-    constructor(points, scale = 10) {
+    constructor(
+
+        points,
+
+        width = 1,
+        height = 1,
+        scale = 1
+
+    ) {
 
         super();
 
-        this._points = points;
-        this._scale = scale;
+        this._path_config = {
+            points: points,
+            width: width,
+            height: height,
+            scale: scale
+        };
 
         this._createCurves();
     }
 
     _createCurves() {
 
-        for ( var i = 0, len = this._points.length; i < len; i++ ) {
+        for ( var i = 0, len = this._path_config.points.length; i < len; i++ ) {
 
-            let next = i === len - 1 ? this._points[ 0 ] : this._points[ i + 1 ];
+            let v1 = this._path_config.points[ i ];
+            let v2 = i === len - 1 ? this._path_config.points[ 0 ] : this._path_config.points[ i + 1 ];
 
-            let line_curve = new LineCurve3( this._points[ i ].multiplyScalar( this._scale ), next );
+            // Apply dimensions
+            v1.x *= this._path_config.width * this._path_config.scale;
+            v1.y *= this._path_config.height * this._path_config.scale;
+            v1.z *= this._path_config.height * this._path_config.scale; // Assume depth == height
+
+            let line_curve = new LineCurve3( v1, v2 );
 
             this.add( line_curve );
         }
